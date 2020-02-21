eded7c373bbb6b74f3048a1f43dd1315d5236548
Cameron Maske <cameronmaske@gmail.com>
2020-02-21 17:56:59 +0100

    Fix when capture overflows

--- miniaudio.py
+++ miniaudio.py
@@ -5,7 +5,7 @@ Author: Irmen de Jong (irmen@razorvine.net)
 Software license: "MIT software license". See http://opensource.org/licenses/MIT
 """
 
-__version__ = "1.10-rc3"
+__version__ = "1.10-rc4"
 
 
 import abc
